4f8dc1aa9dac8ce8ce01fa8d60e826b450e0e09f
jburtson <jburtson@gmail.com>
2026-01-13 15:43:37 -0800

    Differentiated input word list files in main

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import os
 import csv
 
-# word_list_file = 'wordlist.csv'
-word_list_file = 'dictionary.txt'
-word_list = []
+random_word_list = []
+typed_word_list_dictionary = {}
 
 # Read word list into memory
-with open(word_list_file, mode ='r')as file:
-  csvFile = csv.reader(file)
-  for lines in csvFile:
-    word_list.append(lines[0])
-    # print(lines[0])
+def read_random_word_list():
+    word_list_file = 'dictionary.txt'
+    with open(word_list_file, mode ='r')as file:
+        csvFile = csv.reader(file)
+        for lines in csvFile:
+            random_word_list.append(lines[0])
+            # print(lines[0])
+
+def read_labeled_word_list():
+    word_list_file = 'wordlist.csv'
+    with open(word_list_file, mode ='r')as file:
+        csvFile = csv.reader(file)
+        types = lines[3]
+        # TODO: Parse lines[3] for all word types, then add to dictionary for each type, array of words
+        for lines in csvFile:
+            typed_word_list_dictionary[types] = lines[0]
+            print(lines[0])
 
 # Get random int (inclusive) from a range. Uses random bytes.
 def get_random_int(min_value, max_value):
@@ -21,9 +32,9 @@ def get_random_int(min_value, max_value):
 
 # Gets a random word from word list
 def get_random_word():
-    list_size = word_list.__len__()
+    list_size = random_word_list.__len__()
     i = get_random_int(0, list_size-1)
-    return word_list[i]
+    return random_word_list[i]
 
 # Create a sentence of random words from word list, of specified number of words
 def get_random_sentence(length):
